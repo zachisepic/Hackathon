77d25b2fc59b44ed2acfa6940273bf53ae3f9df9
b-i09 <145026434+bpopbeen@users.noreply.github.com>
2026-05-09 14:45:15 -0400

    Replaced catascii.py with dogascii.py, which contains ASCII art of dogs instead of cats. The new file includes various dog poses such as sleeping, happy, chilling, and eating. The old file had similar poses but with cats.

---
+++ dogascii.py
@@ -0,0 +1,64 @@
+alien_sleeping_dog = """
+       __      _,,,---,,_
+ZZZzz /_ \-`'          -.  ;-;;,_
+     |,4-  ) )-,_. ,\ (  `'-'
+    '---''(_/--'  `-'\_)
+"""
+print(alien_sleeping_dog)
+
+
+alien_happy_dog = """
+  / ^ ^ \  (
+ (  o.o  ) _)
+  \  u  / (
+ ( |  | )
+(__d  b__)
+"""
+print(alien_happy_dog)
+
+
+
+alien_chilling_dog = """
+            __..---''``---....___   _..._    __
+ /// //_.-'    .-/";  `        ``<._  ``.''_ `. / // /
+///_.-' _..--.'_    \                    `( ) ) // //
+/ (_..-' // (< _     ;_..__               ; `' / ///
+ / // // //  `-._,_)' // / ``--...____..-' /// / //
+"""
+print(alien_chilling_dog)  
+
+
+
+alien_angry_dog = """
+     _
+    / )
+   / /  
+  / /               
+ / /     .-```-.      / ^`-.  
+ \ \    /       \_   / (|) `o
+  \ \  /   .-.   \\_/  ,--'
+   \ \/   /   )   \(_ `^^^  
+    \  \/    (     )  
+     \  )     )   /    
+      ) /__    | (__  
+     (___)))  (__)))
+"""
+print(alien_angry_dog)
+
+
+
+
+alien_dog_eating = """
+    ,    /-.
+   ((___/ __>
+   /      }
+   \ .--.(    ___
+    \\   \\  /___\
+
+"""
+print(alien_dog_eating)
+
+
+
+
+# https://chatgpt.com/gg/v/69ff51461930819a9ed3149b2f1b0e1c?token=yXVS5f6J3zHR16UfOtGdFA
