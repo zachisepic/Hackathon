9414b6ceac86c978ec64dff9a3520d1d3a5d3cb6
AJGithub1188 <aarevalomayorga@gmail.com>
2026-05-09 11:07:33 -0400

    added relationship attribute to Dog class and implemented check_relationship method to evaluate the bond between the dog and the owner.

--- dogclass.py
+++ dogclass.py
@@ -29,3 +29,7 @@ class VirtualDog:
             print(f"{self.name} is moderately happy.")
         else:
             print(f"{self.name} is not very happy.")
+    def check_relationship(self):
+        if self.relationship >= 100:
+            print(f"You have beeen kind to me in return I'll tell you anything about your home planet!")
+       
