d673357dfd7f591d49642d1a4d74a2a7272f354a
zachisepic <zacharyvigliotti@gmail.com>
2026-05-10 07:00:43 -0400

    added more go ascii

--- Dog.py
+++ Dog.py
@@ -1,5 +1,7 @@
 from dogclass import VirtualDog
+from dogascii import alien_sleeping_dog
 
-name = input("Name your dog: ")
+print(alien_sleeping_dog)
+name = input("Name your Alien: ")
 
 dog = VirtualDog(name)
